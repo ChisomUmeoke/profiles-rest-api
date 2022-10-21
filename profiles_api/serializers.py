eb3e3ece3bb6c99eb365c22303028ab29af59091
Cheesom <chisom.umeoke@gmail.com>
2022-10-21 19:23:59 +0100

    added user profile feed Api

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -40,3 +40,11 @@ class UserProfileSerializer(serializers.ModelSerializer):
         return super().update(instance, validated_data)
 
 
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """Serializes profile feed items"""
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'created_on')
+        extra_kwargs = {'user_profile': {'read_only': True}}
+        """We are setting this User Profile to read only because we are going to set it based on the authenticated user"""
