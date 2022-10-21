91aa7a2eb157f760c340a1a6f956eab4f31de9fd
Cheesom <chisom.umeoke@gmail.com>
2022-10-21 18:10:05 +0100

    add login Api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,9 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
 
 from profiles_api import serializers
 from profiles_api import models
@@ -100,10 +103,14 @@ class HelloViewSet(viewsets.ViewSet):
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
-    """Handle creating and upsating profiles"""
+    """Handle creating and updating profiles"""
     serializer_class= serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication token"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
